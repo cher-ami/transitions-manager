df42d65272212577117613246345f551859f67c8
Willy Brauner <mail@willybrauner.com>
2022-02-16 09:18:46 +0100

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
     root: "_example",
     plugins: [react()],
-  };
-});
+    server: {
+      host: true,
+    },
+  }
+})
