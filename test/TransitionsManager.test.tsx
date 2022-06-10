e7943c909e7c959bcb70131f80c8e6b66138d661
Willy Brauner <mail@willybrauner.com>
2022-06-10 15:23:16 +0200

    Update dependencies

--- test/TransitionsManager.test.tsx
+++ test/TransitionsManager.test.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import {act, render} from "@testing-library/react"
 import {TransitionsManager, usePlayIn, usePlayOut} from "../src"
 import {TransitionsHoc} from "../src"
+
 const {log} = console
 
 
@@ -62,7 +63,8 @@ beforeEach(() => {
       done()
       mockUsePlayOutComplete()
     })
-    return <header className={props.className} data-testid={"header"}>Header</header>
+    return <header className={props.className}
+                   data-testid={"header"}>Header</header>
   }, TestTransitionsManager)
 
 })
@@ -71,8 +73,7 @@ beforeEach(() => {
 
 describe("Transitions Manager", () => {
 
-  it("should respect mount and unmount properly", async () =>
-  {
+  it("should respect mount and unmount properly", async () => {
     expect(TestTransitionsManager.mountStateSignal.state).toBe("unmount")
     TestTransitionsManager.mount()
     expect(TestTransitionsManager.mountStateSignal.state).toBe("mount")
@@ -80,53 +81,58 @@ describe("Transitions Manager", () => {
     expect(TestTransitionsManager.mountStateSignal.state).toBe("unmount")
   })
 
-  it("should mount automatically when playIn", async () =>
-  {
+  it("should mount automatically when playIn", async () => {
+
     expect(TestTransitionsManager.mountStateSignal.state).toBe("unmount")
     expect(TestTransitionsManager.playStateSignal.state).toBe("hidden")
-    // start playIn
-    TestTransitionsManager.playIn()
-    // tricky, the playin method auto dispatch mount and wait for promise is complete
-    // for this test, we need to force de mount complete method
-    // witch should be trigger by useIsMount() hook
-    TestTransitionsManager.mountComplete()
 
-    expect(TestTransitionsManager.mountStateSignal.state).toBe("mount")
-    // because render hack // FIXME
-    await waiting(12)
-    expect(TestTransitionsManager.playStateSignal.state).toBe("play-in")
-    // play in complete method should change play state to visible
-    TestTransitionsManager.playInComplete()
-    expect(TestTransitionsManager.playStateSignal.state).toBe("visible")
+    await act(async () => {
+
+      // start playIn
+      TestTransitionsManager.playIn()
+      // tricky, the playin method auto dispatch mount and wait for promise is complete
+      // for this test, we need to force de mount complete method
+      // witch should be trigger by useIsMount() hook
+      TestTransitionsManager.mountComplete()
+
+      expect(TestTransitionsManager.mountStateSignal.state).toBe("mount")
+      // because render hack // FIXME
+      await waiting(12)
+      expect(TestTransitionsManager.playStateSignal.state).toBe("play-in")
+      // play in complete method should change play state to visible
+      TestTransitionsManager.playInComplete()
+      expect(TestTransitionsManager.playStateSignal.state).toBe("visible")
+    })
   })
 
 
-  it("should respect state cycle using component", async () =>
-  {
+  it("should respect state cycle using component", async () => {
     const transitionDuration = 200
     const wrapper = render(<App transitionDuration={transitionDuration}/>)
     const playInButton = wrapper.getByTestId("play-in")
     const playOutButton = wrapper.getByTestId("play-out")
-    const headerWrapper = wrapper.getByTestId("header-wrapper")
+
     /**
      * Mount + playIn
      */
     // Header is wrapped in container, test if container has child element
-    expect(headerWrapper.firstChild).toBe(null)
+    expect(wrapper.getByTestId("header-wrapper").firstChild).toBe(null)
     // At this step, state should be unmount
     expect(TestTransitionsManager.mountStateSignal.state).toBe("unmount")
 
-    // then, chick play in button
-    playInButton.click()
+      // then, chick play in button
+    await act(async () => {
+      playInButton.click()
 
-    // state is mount
+    })
     expect(TestTransitionsManager.mountStateSignal.state).toBe("mount")
-    // header should exist now, because he his mount
+
     expect(wrapper.getByTestId("header")).toBeDefined()
+
     // but he is already hidden
     expect(TestTransitionsManager.playStateSignal.state).toBe("hidden")
-    // because hack render 10ms
-    await waiting(11)
+    // because hack render 10ms...
+    await waiting(12)
     // now he change to play in state
     expect(TestTransitionsManager.playStateSignal.state).toBe("play-in")
     expect(mockUsePlayIn).toHaveBeenCalledTimes(1)
@@ -145,13 +151,14 @@ describe("Transitions Manager", () => {
       expect(TestTransitionsManager.playStateSignal.state).toBe("play-out")
       expect(mockUsePlayOut).toHaveBeenCalledTimes(1)
       await waiting(transitionDuration)
-      // waiting for transitions end
-      expect(TestTransitionsManager.playStateSignal.state).toBe("hidden")
-      expect(mockUsePlayOutComplete).toHaveBeenCalledTimes(1)
-      // unmount, component is destroy
-      expect(TestTransitionsManager.mountStateSignal.state).toBe("unmount")
-      expect(headerWrapper.firstChild).toBe(null)
     })
+    // waiting for transitions end
+    expect(TestTransitionsManager.playStateSignal.state).toBe("hidden")
+    expect(mockUsePlayOutComplete).toHaveBeenCalledTimes(1)
+    // unmount, component is destroy
+    expect(TestTransitionsManager.mountStateSignal.state).toBe("unmount")
+
+    expect(wrapper.getByTestId("header-wrapper").firstChild).toBe(null)
 
   })
 
